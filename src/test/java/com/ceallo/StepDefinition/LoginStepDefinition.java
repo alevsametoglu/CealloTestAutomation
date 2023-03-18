d2b89580a1c3bf3a6118dd3ddd5f2157c9e7f077
Alev Sametoglu <alevsametoglu.fr@gmail.com>
2023-03-18 22:41:08 +0100

    test login functionality

--- src/test/java/com/ceallo/StepDefinition/LoginStepDefinition.java
+++ src/test/java/com/ceallo/StepDefinition/LoginStepDefinition.java
@@ -1,13 +1,23 @@
 package com.ceallo.StepDefinition;
 
+import com.ceallo.Pages.LoginPage;
+import com.ceallo.Utilities.ConfigurationReader;
+import com.ceallo.Utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDefinition {
+    LoginPage loginpage = new LoginPage();
 
     @Given("user is on the Login page")
     public void user_is_on_the_login_page() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("loginUrl"));
+
+        loginpage.userName.sendKeys(ConfigurationReader.getProperty("loginUsername"));
+        loginpage.password.sendKeys(ConfigurationReader.getProperty("loginPw"));
+        loginpage.loginButton.click();
+
 
     }
 
